71992503fe9e74d3b2e3967694fff0e20aede14d
Daniel Yung-Tse Hsiao <e5q8@ugrad.cs.ubc.ca>
2014-11-23 17:07:22 -0800

    changes to dailyReport, return, and returnResult pages

---
+++ returnResult.php
@@ -0,0 +1,11 @@
+<?php
+    echo "CustomerID: ".$_POST["cid"];
+    echo "ReceiptID: ".$_POST["receiptId"];
+    echo "UPC: ".$_POST["upc"];
+    echo "Quantity: ".$_POST["quantity"];
+    echo "Year: ".$_POST["year"];
+    echo "Month: ".$_POST["month"];
+    echo "Day: ".$_POST["day"];
+    $dateString = "'".$POST["year"]."-".$POST_["month"]."-".$POST_["day"]."'";
+    $queryString = ""
+?>
