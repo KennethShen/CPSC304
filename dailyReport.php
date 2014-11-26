49adbc583d16fb065d07a84a7fd90f8a48e4941e
Daniel Yung-Tse Hsiao <e5q8@ugrad.cs.ubc.ca>
2014-11-26 15:42:12 -0800

    small changes

--- dailyReport.php
+++ dailyReport.php
@@ -6,7 +6,6 @@ if(mysqli_connect_errno()){
     printf("Connection fail: %s\n", mysqli_connect_errno());
     exit();
 }
-
 ?>
 <body>
 <br>
@@ -33,78 +32,82 @@ if(mysqli_connect_errno()){
     if((!isset($_POST["year"])) ||(!isset($_POST["month"])) || (!isset($_POST["day"]))){
         Echo "Please specify a date.<br>";
     } else {
-    $year = $_POST["year"];
-    $month = $_POST["month"];
-    $day = $_POST["day"];
-    $dateString = $year."-".$month."-".$day;
-    $queryString = "SELECT i.upc, i.category, i.price, sum(quantity), sum(quantity)*i.price AS total " .
-        "FROM Purchase p, Item i, PurchaseItem pi " .
-        "WHERE p.date = ? AND pi.upc = i.upc AND pi.receiptId = p.receiptId
+    $year = intval($_POST["year"]);
+    $month = intval($_POST["month"]);
+    $day = intval($_POST["day"]);
+        if(checkdate($month,$day,$year)) {
+            $dateString = $year . "-" . $month . "-" . $day;
+            $queryString = "SELECT i.upc, i.category, i.price, sum(quantity), sum(quantity)*i.price AS total " .
+                "FROM Purchase p, Item i, PurchaseItem pi " .
+                "WHERE p.date = ? AND pi.upc = i.upc AND pi.receiptId = p.receiptId
          GROUP BY i.upc, i.category ORDER BY i.category, total DESC";
-    $stmt = $connection->prepare($queryString);
-    $stmt->bind_param("s", $dateString);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    if($result == NULL){
-        die('Error running the query.');
-    }
-    $i = 0;
-    $totalCatUnit = 0;
-    $totalCatSale = 0;
-    $totalUnit = 0;
-    $totalSale = 0;
-    while($row = $result->fetch_assoc()){
-        if($i == 0){
-            $lastCat = $row['category'];
-        }
-        if($lastCat != $row['category']) {
+            $stmt = $connection->prepare($queryString);
+            $stmt->bind_param("s", $dateString);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            if ($result == NULL) {
+                die('Error running the query.');
+            }
+            $i = 0;
+            $totalCatUnit = 0;
+            $totalCatSale = 0;
+            $totalUnit = 0;
+            $totalSale = 0;
+            while ($row = $result->fetch_assoc()) {
+                if ($i == 0) {
+                    $lastCat = $row['category'];
+                }
+                if ($lastCat != $row['category']) {
+                    echo "<tr>";
+                    echo "<td>" . "</td>";
+                    echo "<td>" . "Total" . "</td>";
+                    echo "<td>" . "</td>";
+                    echo "<td>" . $totalCatUnit . "</td>";
+                    echo "<td>" . $totalCatSale . "</td>";
+                    $totalCatSale = 0;
+                    $totalCatUnit = 0;
+                }
+                echo "<tr>";
+                echo "<td>" . $row['upc'] . "</td>";
+                echo "<td>" . $row['category'] . "</td>";
+                echo "<td>" . $row['price'] . "</td>";
+                echo "<td>" . $row['sum(quantity)'] . "</td>";
+                echo "<td>" . $row['total'] . "</td>";
+                echo "</tr>";
+                $totalCatUnit += (double)$row['sum(quantity)'];
+                $totalCatSale += (double)$row['total'];
+                $lastCat = $row['category'];
+                $i += 1;
+                $totalUnit += (double)$row['sum(quantity)'];
+                $totalSale += (double)$row['total'];
+            }
             echo "<tr>";
-            echo "<td>"."</td>";
-            echo "<td>"."Total"."</td>";
-            echo "<td>"."</td>";
-            echo "<td>".$totalCatUnit."</td>";
-            echo "<td>".$totalCatSale."</td>";
+            echo "<td>" . "</td>";
+            echo "<td>" . "Total" . "</td>";
+            echo "<td>" . "</td>";
+            echo "<td>" . $totalCatUnit . "</td>";
+            echo "<td>" . $totalCatSale . "</td>";
             $totalCatSale = 0;
             $totalCatUnit = 0;
+            echo "<tr>";
+            echo "<td>" . "</td>";
+            echo "<td>" . "</td>";
+            echo "<td>" . "</td>";
+            echo "<td>" . "</td>";
+            echo "<td>" . "----------" . "</td>";
+            echo "</tr>";
+            echo "<tr>";
+            echo "<td>" . "</td>";
+            echo "<td>" . "Total Daily" . "</td>";
+            echo "<td>" . "Sales" . "</td>";
+            echo "<td>" . $totalUnit . "</td>";
+            echo "<td>" . $totalSale . "</td>";
+            echo "</tr>";
+            echo "</table>";
+            mysqli_close($connection);
+        } else {
+            echo "Please enter a valid date. <br>";
         }
-        echo "<tr>";
-        echo "<td>".$row['upc']."</td>";
-        echo "<td>".$row['category']."</td>";
-        echo "<td>".$row['price']."</td>";
-        echo "<td>".$row['sum(quantity)']."</td>";
-        echo "<td>".$row['total']."</td>";
-        echo "</tr>";
-        $totalCatUnit += (double) $row['sum(quantity)'];
-        $totalCatSale += (double) $row['total'];
-        $lastCat = $row['category'];
-        $i += 1;
-        $totalUnit += (double) $row['sum(quantity)'];
-        $totalSale += (double) $row['total'];
-    }
-    echo "<tr>";
-    echo "<td>"."</td>";
-    echo "<td>"."Total"."</td>";
-    echo "<td>"."</td>";
-    echo "<td>".$totalCatUnit."</td>";
-    echo "<td>".$totalCatSale."</td>";
-    $totalCatSale = 0;
-    $totalCatUnit = 0;
-    echo "<tr>";
-    echo "<td>"."</td>";
-    echo "<td>"."</td>";
-    echo "<td>"."</td>";
-    echo "<td>"."</td>";
-    echo "<td>"."----------"."</td>";
-    echo "</tr>";
-    echo "<tr>";
-    echo "<td>"."</td>";
-    echo "<td>"."Total Daily"."</td>";
-    echo "<td>"."Sales"."</td>";
-    echo "<td>".$totalUnit."</td>";
-    echo "<td>".$totalSale."</td>";
-    echo "</tr>";
-    echo "</table>";
-    mysqli_close($connection);
     }
     ?>
 </table>
--- return.php
+++ return.php
@@ -1,11 +1,7 @@
 <?php
 require_once("includes/connection.php");
 include_once("includes/header.php");
-    function inputCheck($string, $type){
-        if($type == "year"){
 
-        }
-    }
 ?>
 
 <body>
@@ -13,12 +9,12 @@ include_once("includes/header.php");
 <form action = "" method = "post">
     Customer ID: <input type = "text" name = "cid"><br>
     Receipt ID: <input type = "text" name = "receiptId"><br>
-    Item UPC: <input type = "text" name = "UPC"><br>
-    Quantity: <input type = "text" name = "returnQuantity"><br>
+    Item UPC  : <input type = "text" name = "UPC"><br>
+    Quantity  : <input type = "text" name = "returnQuantity"><br>
     <input type = "submit">
 </form>
 <?php
-echo "Please enter your Customer ID, Receipt ID, UPC of the item you wish to return, and the quantity of the item that you are returning.<br>";
+echo "Please enter your Customer ID, Receipt ID, UPC of the item you wish to return, <br> and the quantity of the item that you are returning.<br>";
 if(!isset($_POST["cid"]) || !isset($_POST["receiptId"]) || !isset($_POST["UPC"]) || !isset($_POST["returnQuantity"])){
 } else {
     $cid = intval($_POST["cid"]);
@@ -26,36 +22,47 @@ if(!isset($_POST["cid"]) || !isset($_POST["receiptId"]) || !isset($_POST["UPC"])
     $upc = intval($_POST["UPC"]);
     $quantity = intval($_POST["returnQuantity"]);
     $queryString = "SELECT pi.receiptId, pi.quantity, p.date FROM Purchase p, PurchaseItem pi, Item i WHERE p.receiptId = pi.receiptId" .
-        " AND i.upc = pi.upc AND pi.upc = ? AND pi.receiptId = ? AND p.cid = ? AND p.date > CURRENT_DATE() - 15";
+        " AND i.upc = pi.upc AND pi.upc = ? AND pi.receiptId = ? AND p.cid = ?";
     $stmt = $connection->prepare($queryString);
     $stmt->bind_param("iii", $upc, $rid, $cid);
     $stmt->execute();
     $result = $stmt->get_result();
     $search = $result->fetch_assoc();
     if ($search == NULL) {
-        echo "The Purchase Date is more than 15 days ago or No purchase found for given UPC, Receipt ID, and Customer ID.<br>";
+        echo "<br>No purchase found for the given UPC, Receipt ID, and Customer ID.<br>";
     } else {
-        if ($quantity > $search['quantity']) {
-            echo "Returning quantity is greater than purchased quantity.<br>";
-        } else if ($quantity < 1) {
-            echo "Please enter a quantity larger than 0.<br>";
+        $queryString = "SELECT pi.receiptId, pi.quantity, p.date FROM Purchase p, PurchaseItem pi, Item i WHERE p.receiptId = pi.receiptId" .
+            " AND i.upc = pi.upc AND pi.upc = ? AND pi.receiptId = ? AND p.cid = ? AND p.date > CURRENT_DATE() - 15";
+        $stmt = $connection->prepare($queryString);
+        $stmt->bind_param("iii", $upc, $rid, $cid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $search = $result->fetch_assoc();
+        if($search != NULL){
+            if ($quantity > $search['quantity']) {
+                echo "Returning quantity is greater than purchased quantity.<br>";
+            } else if ($quantity < 1) {
+                echo "Please enter a quantity larger than 0.<br>";
+            } else {
+                $returnString = "INSERT INTO returned (retid, date, receiptId) VALUES (NULL,CURRENT_DATE(),?)";
+                $retSt = $connection->prepare($returnString);
+                $retSt->bind_param("i", $rid);
+                $retSt->execute();
+                $returnID = $connection->insert_id;
+                echo "<br>Your Return ID  is: " . $returnID . "<br>";
+                $returnItemString = "INSERT INTO returnItem(retid, upc, quantity) VALUES (?,?,?)";
+                $riStmt = $connection->prepare($returnItemString);
+                $riStmt->bind_param("iii", $returnID, $upc, $quantity);
+                $riStmt->execute();
+                echo "The refund amount has been issued to your credit card.<br>";
+                $updateString = "UPDATE PurchaseItem SET receiptId = receiptId, upc = upc, quantity = ? WHERE receiptId = ? AND upc = ?";
+                $newQuantity = $search['quantity'] - $quantity;
+                $updateStmt = $connection->prepare($updateString);
+                $updateStmt->bind_param('iii', $newQuantity, $rid, $upc);
+                $updateStmt->execute();
+            }
         } else {
-            $returnString = "INSERT INTO returned (retid, date, receiptId) VALUES (NULL,CURRENT_DATE(),?)";
-            $retSt = $connection->prepare($returnString);
-            $retSt->bind_param("i", $rid);
-            $retSt->execute();
-            $returnID = $connection->insert_id;
-            echo "Your Return ID  is: " . $returnID . "<br>";
-            $returnItemString = "INSERT INTO returnItem(retid, upc, quantity) VALUES (?,?,?)";
-            $riStmt = $connection->prepare($returnItemString);
-            $riStmt->bind_param("iii", $returnID, $upc, $quantity);
-            $riStmt->execute();
-            echo "The refund amount has been issued to your credit card.<br>";
-            $updateString = "UPDATE PurchaseItem SET receiptId = receiptId, upc = upc, quantity = ? WHERE receiptId = ? AND upc = ?";
-            $newQuantity = $search['quantity'] - $quantity;
-            $updateStmt = $connection->prepare($updateString);
-            $updateStmt->bind_param('iii', $newQuantity, $rid, $upc);
-            $updateStmt->execute();
+            echo "The purchase was made more than 15 days ago. <br>";
         }
     }
 }
